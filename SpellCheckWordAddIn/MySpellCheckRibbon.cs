b6fdbf8eb1ff87f2db969fed19ab2af44b838878
Kareem Sulthan <itsmystyle.shaik@hotmail.com>
2023-10-12 10:53:53 +0530

    Add project files.

---
+++ SpellCheckWordAddIn/MySpellCheckRibbon.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using Office = Microsoft.Office.Core;
+using Forms = System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+using System.Net.NetworkInformation;
+
+// TODO:  Follow these steps to enable the Ribbon (XML) item:
+
+// 1: Copy the following code block into the ThisAddin, ThisWorkbook, or ThisDocument class.
+
+//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+//  {
+//      return new MySpellCheckRibbon();
+//  }
+
+// 2. Create callback methods in the "Ribbon Callbacks" region of this class to handle user
+//    actions, such as clicking a button. Note: if you have exported this Ribbon from the Ribbon designer,
+//    move your code from the event handlers to the callback methods and modify the code to work with the
+//    Ribbon extensibility (RibbonX) programming model.
+
+// 3. Assign attributes to the control tags in the Ribbon XML file to identify the appropriate callback methods in your code.  
+
+// For more information, see the Ribbon XML documentation in the Visual Studio Tools for Office Help.
+
+
+namespace SpellCheckWordAddIn
+{
+    [ComVisible(true)]
+    public class MySpellCheckRibbon : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+
+        public MySpellCheckRibbon()
+        {
+        }
+
+        public void openSpellCheckButton_onAction(Office.IRibbonControl control)
+        {
+            try
+            {
+                SpellCheckUserControl userControl = new SpellCheckUserControl();
+
+                Forms.Integration.ElementHost elementHost = new Forms.Integration.ElementHost()
+                {
+                    Dock = Forms.DockStyle.Fill,
+                    Child = userControl,
+                };
+
+                Forms.UserControl wfUserControl = new Forms.UserControl()
+                {
+                    Font = new System.Drawing.Font("Calibri", 10f, System.Drawing.FontStyle.Regular),
+                };
+                wfUserControl.Controls.Add(elementHost);
+
+                var taskPane = Globals.ThisAddIn.CustomTaskPanes.Add(wfUserControl, "My Spell Check");
+                taskPane.Width = 450;
+                taskPane.DockPositionRestrict = Office.MsoCTPDockPositionRestrict.msoCTPDockPositionRestrictNoHorizontal;
+                taskPane.DockPosition = Office.MsoCTPDockPosition.msoCTPDockPositionRight;
+                taskPane.Visible = true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            return GetResourceText("SpellCheckWordAddIn.MySpellCheckRibbon.xml");
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+        //Create callback methods here. For more information about adding callback methods, visit https://go.microsoft.com/fwlink/?LinkID=271226
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ SpellCheckWordAddIn/SpellCheckUserControl.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Forms = System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+
+namespace SpellCheckWordAddIn
+{
+    /// <summary>
+    /// Interaction logic for SpellCheckUserControl.xaml
+    /// </summary>
+    public partial class SpellCheckUserControl : UserControl
+    {
+        private ObservableCollection<SpellError> spellErrors;
+
+        public SpellCheckUserControl()
+        {
+            InitializeComponent();
+
+            spellErrors = new ObservableCollection<SpellError>();
+            spellErrorsItemsControl.ItemsSource = spellErrors;
+
+            this.Loaded += SpellCheckUserControl_Loaded;
+        }
+
+        private void SpellCheckUserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Load();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void refreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Load();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < spellErrors.Count; i++)
+                {
+                    spellErrors[i].Range.Text = spellErrors[i].Text;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void gotoButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Button button = (Button)sender;
+                if (button.DataContext is SpellError spellError)
+                {
+                    spellError.Range.Select();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Load()
+        {
+            try
+            {
+                spellErrors.Clear();
+
+                Word.Application application = Globals.ThisAddIn.Application;
+                Word.Document activeDocument = application.ActiveDocument;
+                for (int i = 1; i <= activeDocument.Content.SpellingErrors.Count; i++)
+                {
+                    SpellError spellError = new SpellError()
+                    {
+                        Text = activeDocument.Content.SpellingErrors[i].Text,
+                        Range = activeDocument.Content.SpellingErrors[i],
+                    };
+
+                    spellErrors.Add(spellError);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+       
+    }
+
+
+    public class SpellError
+    {
+        public string Text { get; set; }
+
+        public Word.Range Range { get; set; }
+    }
+}
